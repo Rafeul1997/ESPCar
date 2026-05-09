283a0e801db4052d0f4ad4a8bbf4dda2154c51a7
Guru of Microcontroller <118795462+Rafeul1997@users.noreply.github.com>
2026-05-09 23:45:57 +0530

    Fix formatting and add Serial.begin to setup

--- examples/BT_CAR/BT_CAR.ino
+++ examples/BT_CAR/BT_CAR.ino
@@ -1,24 +1,22 @@
 #include <ESPCar.h>
 
-// Motor pins: IN1, IN2, IN3, IN4
 ESPCar car(5, 18, 19, 21);
 
 char cmd;
 
 void setup() {
-  Serial.begin(9600);   // Bluetooth uses Serial
   car.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
   if (Serial.available()) {
     cmd = Serial.read();
 
-    // Control commands from mobile app
-    if (cmd == 'F') car.forward();   // Forward
-    else if (cmd == 'B') car.backward(); // Backward
-    else if (cmd == 'L') car.left(); // Left
-    else if (cmd == 'R') car.right(); // Right
-    else if (cmd == 'S') car.stop(); // Stop
+    if (cmd == 'F') car.forward();
+    else if (cmd == 'B') car.backward();
+    else if (cmd == 'L') car.left();
+    else if (cmd == 'R') car.right();
+    else if (cmd == 'S') car.stop();
   }
 }
