e2792e9e1f8d7bf4ed1691d21b42bbaca8813774
Guru of Microcontroller <118795462+Rafeul1997@users.noreply.github.com>
2026-05-09 23:47:47 +0530

    Update WIFI_SPEED_CONTROL.ino

--- examples/WIFI_SPEED_CONTROL/WIFI_SPEED_CONTROL.ino
+++ examples/WIFI_SPEED_CONTROL/WIFI_SPEED_CONTROL.ino
@@ -1,59 +1,34 @@
-#include <WiFi.h>
 #include <ESPCar.h>
 
 ESPCar car(5, 18, 19, 21, 23, 22);
 
-const char* ssid = "ESP-CAR";
-const char* password = "12345678";
-
-WiFiServer server(80);
-
+String req;
 int speedValue = 150;
 
 void setup() {
   car.begin();
   car.setSpeed(speedValue);
-
-  WiFi.softAP(ssid, password);
-  server.begin();
+  car.beginWiFi("ESP-CAR", "12345678");
 }
 
 void loop() {
-  WiFiClient client = server.available();
+  req = car.getWiFiRequest();
 
-  if (client) {
-    String request = client.readStringUntil('\r');
-    client.flush();
+  if (req != "") {
 
-    if (request.indexOf("/F") != -1) car.forward();
-    else if (request.indexOf("/B") != -1) car.backward();
-    else if (request.indexOf("/L") != -1) car.left();
-    else if (request.indexOf("/R") != -1) car.right();
-    else if (request.indexOf("/S") != -1) car.stop();
+    if (req.indexOf("/F") != -1) car.forward();
+    else if (req.indexOf("/B") != -1) car.backward();
+    else if (req.indexOf("/L") != -1) car.left();
+    else if (req.indexOf("/R") != -1) car.right();
+    else if (req.indexOf("/S") != -1) car.stop();
 
-    // Speed control via URL like /SPEED?val=200
-    int idx = request.indexOf("val=");
+    int idx = req.indexOf("val=");
     if (idx != -1) {
-      speedValue = request.substring(idx + 4).toInt();
+      speedValue = req.substring(idx + 4).toInt();
       speedValue = constrain(speedValue, 0, 255);
       car.setSpeed(speedValue);
     }
 
-    client.println("HTTP/1.1 200 OK");
-    client.println("Content-type:text/html");
-    client.println();
-
-    client.println("<h1>ESP CAR SPEED CONTROL</h1>");
-    client.println("<a href='/F'>Forward</a><br>");
-    client.println("<a href='/B'>Backward</a><br>");
-    client.println("<a href='/L'>Left</a><br>");
-    client.println("<a href='/R'>Right</a><br>");
-    client.println("<a href='/S'>Stop</a><br>");
-
-    client.println("<br>Set Speed:<br>");
-    client.println("<a href='/SPEED?val=100'>Speed 100</a><br>");
-    client.println("<a href='/SPEED?val=200'>Speed 200</a><br>");
-
-    client.stop();
+    car.sendWiFiResponse();
   }
 }
