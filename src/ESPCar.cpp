2ef34b36579a300caf9de8f37082db624cc7d534
Guru of Microcontroller <118795462+Rafeul1997@users.noreply.github.com>
2026-05-09 23:45:17 +0530

    Update ESPCar.cpp

--- src/ESPCar.cpp
+++ src/ESPCar.cpp
@@ -1,79 +1,111 @@
 #include "ESPCar.h"
+#include <WiFi.h>
 
-// Constructor
-ESPCar::ESPCar(int in1, int in2, int in3, int in4, int enA, int enB) {
-  _in1 = in1;
-  _in2 = in2;
-  _in3 = in3;
-  _in4 = in4;
-  _enA = enA;
-  _enB = enB;
-  _speed = 255;
+// WiFi server (internal)
+WiFiServer server(80);
+WiFiClient client;
+
+// Constructors
+ESPCar::ESPCar(int in1, int in2, int in3, int in4) {
+  _in1 = in1; _in2 = in2; _in3 = in3; _in4 = in4;
+  _usePWM = false;
 }
 
-// Initialize pins
+ESPCar::ESPCar(int in1, int in2, int in3, int in4, int ena, int enb) {
+  _in1 = in1; _in2 = in2; _in3 = in3; _in4 = in4;
+  _ena = ena; _enb = enb;
+  _usePWM = true;
+}
+
+// Setup
 void ESPCar::begin() {
   pinMode(_in1, OUTPUT);
   pinMode(_in2, OUTPUT);
   pinMode(_in3, OUTPUT);
   pinMode(_in4, OUTPUT);
 
-  if (_enA != -1) pinMode(_enA, OUTPUT);
-  if (_enB != -1) pinMode(_enB, OUTPUT);
+  if (_usePWM) {
+    pinMode(_ena, OUTPUT);
+    pinMode(_enb, OUTPUT);
+  }
+
+  stop();
 }
 
-// Forward
-void ESPCar::forward() {
-  digitalWrite(_in1, HIGH);
-  digitalWrite(_in2, LOW);
-  digitalWrite(_in3, HIGH);
-  digitalWrite(_in4, LOW);
-
-  setSpeed(_speed);
+// Movement logic
+void ESPCar::move(int a, int b, int c, int d) {
+  digitalWrite(_in1, a);
+  digitalWrite(_in2, b);
+  digitalWrite(_in3, c);
+  digitalWrite(_in4, d);
 }
 
-// Backward
-void ESPCar::backward() {
-  digitalWrite(_in1, LOW);
-  digitalWrite(_in2, HIGH);
-  digitalWrite(_in3, LOW);
-  digitalWrite(_in4, HIGH);
+void ESPCar::forward() { move(HIGH, LOW, HIGH, LOW); }
+void ESPCar::backward(){ move(LOW, HIGH, LOW, HIGH); }
+void ESPCar::left()    { move(LOW, HIGH, HIGH, LOW); }
+void ESPCar::right()   { move(HIGH, LOW, LOW, HIGH); }
+void ESPCar::stop()    { move(LOW, LOW, LOW, LOW); }
 
-  setSpeed(_speed);
-}
-
-// Left
-void ESPCar::left() {
-  digitalWrite(_in1, LOW);
-  digitalWrite(_in2, HIGH);
-  digitalWrite(_in3, HIGH);
-  digitalWrite(_in4, LOW);
-
-  setSpeed(_speed);
-}
-
-// Right
-void ESPCar::right() {
-  digitalWrite(_in1, HIGH);
-  digitalWrite(_in2, LOW);
-  digitalWrite(_in3, LOW);
-  digitalWrite(_in4, HIGH);
-
-  setSpeed(_speed);
-}
-
-// Stop
-void ESPCar::stop() {
-  digitalWrite(_in1, LOW);
-  digitalWrite(_in2, LOW);
-  digitalWrite(_in3, LOW);
-  digitalWrite(_in4, LOW);
-}
-
-// Speed control
+// Speed
 void ESPCar::setSpeed(int speed) {
   _speed = constrain(speed, 0, 255);
 
-  if (_enA != -1) analogWrite(_enA, _speed);
-  if (_enB != -1) analogWrite(_enB, _speed);
+  if (_usePWM) {
+    analogWrite(_ena, _speed);
+    analogWrite(_enb, _speed);
+  }
+}
+
+// BT Debug
+void ESPCar::beginBTDebug(long baud) {
+  Serial.begin(baud);
+}
+
+void ESPCar::handleBTDebug() {
+  if (Serial.available()) {
+    char cmd = Serial.read();
+    Serial.print("Received: ");
+    Serial.println(cmd);
+  }
+}
+
+// WiFi Setup
+void ESPCar::beginWiFi(const char* ssid, const char* password) {
+  WiFi.softAP(ssid, password);
+  server.begin();
+}
+
+// Get Request
+String ESPCar::getWiFiRequest() {
+  client = server.available();
+
+  if (client) {
+    String req = client.readStringUntil('\r');
+    client.flush();
+    return req;
+  }
+  return "";
+}
+
+// Send Response
+void ESPCar::sendWiFiResponse() {
+  if (client) {
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-type:text/html");
+    client.println();
+
+    client.println("<h1>ESP CAR</h1>");
+    client.println("<a href='/F'>F</a><br>");
+    client.println("<a href='/B'>B</a><br>");
+    client.println("<a href='/L'>L</a><br>");
+    client.println("<a href='/R'>R</a><br>");
+    client.println("<a href='/S'>S</a><br>");
+
+    client.println("<br>Speed:<br>");
+    client.println("<a href='/SPEED?val=100'>100</a><br>");
+    client.println("<a href='/SPEED?val=200'>200</a><br>");
+    client.println("<a href='/SPEED?val=255'>MAX</a><br>");
+
+    client.stop();
+  }
 }
