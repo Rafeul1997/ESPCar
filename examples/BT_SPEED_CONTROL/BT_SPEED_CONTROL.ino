3c89806b9c99cdba4685bc6288a3610204b66299
Guru of Microcontroller <118795462+Rafeul1997@users.noreply.github.com>
2026-05-09 23:47:11 +0530

    Update BT_SPEED_CONTROL.ino

--- examples/BT_SPEED_CONTROL/BT_SPEED_CONTROL.ino
+++ examples/BT_SPEED_CONTROL/BT_SPEED_CONTROL.ino
@@ -1,14 +1,13 @@
 #include <ESPCar.h>
 
-// IN1, IN2, IN3, IN4, ENA, ENB
 ESPCar car(5, 18, 19, 21, 23, 22);
 
 char cmd;
-int speedValue = 150; // default speed
+int speedValue = 150;
 
 void setup() {
-  Serial.begin(9600);
   car.begin();
+  Serial.begin(9600);
   car.setSpeed(speedValue);
 }
 
@@ -22,7 +21,6 @@ void loop() {
     else if (cmd == 'R') car.right();
     else if (cmd == 'S') car.stop();
 
-    // Speed control (0–9)
     else if (cmd >= '0' && cmd <= '9') {
       speedValue = map(cmd - '0', 0, 9, 0, 255);
       car.setSpeed(speedValue);
